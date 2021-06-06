271002a12bc764975eb3ef2cdb86251dc47ecd5a
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2021-06-06 13:06:14 +0200

    Implement link shortening

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ltekieli/shorturl
 
 go 1.16
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/matoous/go-nanoid/v2 v2.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
-	"io/ioutil"
+	nanoid "github.com/matoous/go-nanoid/v2"
+	"io"
 	"log"
 	"net/http"
 )
+
+const RequestLimit = 2048
+
+var MappingLongToShort map[string]string
+var MappingShortToLong map[string]string
 
 type LongLink struct {
 	Url string `json:"url"`
@@ -17,25 +23,59 @@ type ShortLink struct {
 }
 
 func shorten(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
+	reqBody, err := io.ReadAll(io.LimitReader(r.Body, RequestLimit))
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
 	var longLink LongLink
-	json.Unmarshal(reqBody, &longLink)
+	err = json.Unmarshal(reqBody, &longLink)
+	if err != nil {
+		http.Error(w, "Invalid long link request", http.StatusInternalServerError)
+	}
 	log.Print(longLink)
-	shortLink := ShortLink{Url: "35gdfgsde"}
-	json.NewEncoder(w).Encode(shortLink)
+
+	cached, ok := MappingLongToShort[longLink.Url]
+	if ok {
+		shortLink := ShortLink{Url: cached}
+		json.NewEncoder(w).Encode(shortLink)
+	} else {
+		sid, _ := nanoid.New()
+		MappingLongToShort[longLink.Url] = sid
+		MappingShortToLong[sid] = longLink.Url
+		shortLink := ShortLink{Url: sid}
+		json.NewEncoder(w).Encode(shortLink)
+	}
 }
 
 func resolve(w http.ResponseWriter, r *http.Request) {
-	reqBody, _ := ioutil.ReadAll(r.Body)
+	reqBody, err := io.ReadAll(io.LimitReader(r.Body, RequestLimit))
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
 	var shortLink ShortLink
-	json.Unmarshal(reqBody, &shortLink)
+	err = json.Unmarshal(reqBody, &shortLink)
+	if err != nil {
+		http.Error(w, "Invalid short link request", http.StatusInternalServerError)
+	}
 	log.Print(shortLink)
-	longLink := ShortLink{Url: "http://page.com"}
-	json.NewEncoder(w).Encode(longLink)
+
+	cached, ok := MappingShortToLong[shortLink.Url]
+	if ok {
+		longLink := ShortLink{Url: cached}
+		json.NewEncoder(w).Encode(longLink)
+	} else {
+		http.Error(w, "Short link does not exist", http.StatusInternalServerError)
+	}
 }
 
 func main() {
 	log.Printf("Starting URL shortener...")
+	MappingLongToShort = make(map[string]string)
+	MappingShortToLong = make(map[string]string)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api/shorten", shorten).Methods("POST")
 	router.HandleFunc("/api/resolve", resolve).Methods("POST")
