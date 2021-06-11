650e2af23a075a15ba34f9722490d066fdb0ebd1
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2021-06-11 19:05:16 +0200

    Add integration test script

---
+++ test/integration.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker run --rm --name mongo1 -d -v /home/tekieli/workspace/mongo_db_data/:/data/db mongo:latest
+docker run --rm --name memcache1 -d memcached:latest memcached -m 64 -vvv
+
+./shorturl &
+
+sleep 3
+
+curl -v --data '{"url": "http://example.com"}' localhost:8080/api/shorten
+
+killall shorturl
+
+docker stop mongo1
+docker stop memcache1
