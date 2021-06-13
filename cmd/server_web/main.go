80234d8b54dbf877e6a26a545939ef5411d16bea
Lukasz Tekieli <tekieli.lukasz@gmail.com>
2021-06-13 15:00:42 +0200

    Add server_web app

---
+++ cmd/server_web/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+
+	"github.com/ltekieli/shorturl/log"
+)
+
+const RequestLimit = 2048
+
+type Config struct {
+	port      *uint
+	static    *string
+	apiServer *string
+}
+
+var (
+	gConfig Config
+	gServer *http.Server
+)
+
+type LongLink struct {
+	Url string `json:"url"`
+}
+
+type ShortLink struct {
+	Url string `json:"url"`
+}
+
+func resolve(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	shortLink := ShortLink{Url: vars["shortid"]}
+	b, err := json.Marshal(shortLink)
+	if err != nil {
+		log.Errorf("Cannot serialize short link: %s", err)
+		http.Error(w, "Cannot serialize short link", http.StatusInternalServerError)
+		return
+	}
+
+	resp, err := http.Post(fmt.Sprintf("http://%s/api/resolve", *gConfig.apiServer), "application/json", bytes.NewReader(b))
+	if err != nil {
+		log.Errorf("Failed to resolve short id: %s", err)
+		http.Error(w, "Failed to resolve short id", http.StatusInternalServerError)
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		log.Error("Failed to resolve short id")
+		http.Error(w, "Failed to resolve short id", http.StatusInternalServerError)
+		return
+	}
+
+	reqBody, err := io.ReadAll(io.LimitReader(resp.Body, RequestLimit))
+	if err != nil {
+		log.Errorf("Failed to read resolve response: %s", err)
+		http.Error(w, "Failed to read resolve response", http.StatusInternalServerError)
+		return
+	}
+
+	var longLink LongLink
+	err = json.Unmarshal(reqBody, &longLink)
+	if err != nil {
+		log.Errorf("Resolve response contains invalid LongLink: %s", err)
+		http.Error(w, "Resolve response contains invalid LongLink", http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, r, longLink.Url, http.StatusSeeOther)
+}
+
+func startServer(ip string, port uint16) {
+	router := mux.NewRouter().StrictSlash(true)
+	router.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(*gConfig.static)))).Methods("GET")
+	router.HandleFunc("/{shortid}", resolve).Methods("GET")
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir(*gConfig.static))).Methods("GET")
+
+	gServer = &http.Server{Addr: fmt.Sprintf("%s:%d", ip, port), Handler: router}
+
+	go func() {
+		log.Info("Serving requests")
+		if err := gServer.ListenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				log.Info("Server shutdown")
+			} else {
+				log.Errorf("Server error during runtime: %s", err)
+				panic(err)
+			}
+		}
+	}()
+}
+
+func stopServer() {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := gServer.Shutdown(ctx); err != nil {
+		log.Errorf("Server error during shutdown: %s", err)
+	}
+}
+
+func waitForInterrupt() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	func() {
+		for {
+			select {
+			case <-stop:
+				return
+			}
+		}
+	}()
+}
+
+func getConfig() {
+	gConfig = Config{}
+	gConfig.port = flag.Uint("port", 8080, "Server port.")
+	gConfig.static = flag.String("static", "", "Static content directory. (Required)")
+	gConfig.apiServer = flag.String("api-server", "", "IP and port of the API server. (Required)")
+	flag.Parse()
+
+	if *gConfig.static == "" || *gConfig.apiServer == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	getConfig()
+	log.Info("Starting URL shortener web...")
+	startServer("0.0.0.0", uint16(*gConfig.port))
+	defer stopServer()
+	waitForInterrupt()
+}
